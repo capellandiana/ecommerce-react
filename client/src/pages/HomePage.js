44785cc4f7d651f9b028c4886828d75e69674a1e
capellandiana <capellandiana@yahoo.com>
2023-04-03 09:42:47 -0400

    adding description

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -8,10 +8,10 @@ import ValueCards from '../components/Home/ValueCards';
 
 function HomePage () {
     const slides = [
-        {url: 'https://images.unsplash.com/photo-1589883661923-6476cb0ae9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2874&q=80', title: 'upsidecat'},
-        {url: 'https://images.unsplash.com/photo-1527416876370-fb74d128c3dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', title: 'kots2'},
-        {url: 'https://images.unsplash.com/photo-1557246565-8a3d3ab5d7f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', title: 'cat1'},
-        {url: 'https://images.unsplash.com/photo-1542297056134-df64a72eaeb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', title: 'meow2'}
+        {url: 'https://images.unsplash.com/photo-1589883661923-6476cb0ae9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2874&q=80', title: 'cat laying down belly up'},
+        {url: 'https://images.unsplash.com/photo-1527416876370-fb74d128c3dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', title: 'two cats'},
+        {url: 'https://images.unsplash.com/photo-1557246565-8a3d3ab5d7f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', title: 'white playful cat'},
+        {url: 'https://images.unsplash.com/photo-1542297056134-df64a72eaeb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', title: 'cat with girl laying down'}
     ]
 
     const containerStyles = {
